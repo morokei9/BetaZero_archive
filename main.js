a9d3275280f592f9055c9fd898a67e833f07435e
MOROKEI <77580227+morokei9@users.noreply.github.com>
2023-04-10 06:53:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const loadEpisodesButton = document.getElementById('load-episodes');
 const episodeList = document.getElementById('episode-list');
 
 loadEpisodesButton.addEventListener('click', async () => {
-  const episodes = await fetchPGNData('2023년 04월 10일_PGN_LOG.txt');
+  const episodes = await fetchPGNData('2023-04-10_PGN_LOG.txt');
   
   episodeList.innerHTML = '';
   episodes.forEach((pgnData, index) => {
@@ -15,8 +15,45 @@ loadEpisodesButton.addEventListener('click', async () => {
   });
 });
 
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+    reader.readAsText(file);
+  });
+}
+
+async function displayGames(fileInput) {
+  const file = fileInput.files[0];
+  const content = await readFile(file);
+
+  const games = content.split("\n\n\n").filter((game) => game.trim() !== "");
+  const container = document.getElementById("games");
+
+  games.forEach((game, index) => {
+    const boardContainer = document.createElement("div");
+    boardContainer.id = `board-${index}`;
+    container.appendChild(boardContainer);
+
+    const chess = new Chess();
+    chess.load_pgn(game);
+
+    const config = {
+      position: chess.fen(),
+      draggable: false,
+    };
+
+    const board = Chessboard(boardContainer.id, config);
+
+    const gameInfo = document.createElement("p");
+    gameInfo.innerHTML = `게임 ${index + 1}: ${chess.header("White")} 대 ${chess.header("Black")} - 결과: ${chess.header("Result")}`;
+    container.appendChild(gameInfo);
+  });
+}
+
 async function fetchPGNData(filename) {
-  const response = await fetch('/pgn txt/' + filename);
+  const response = await fetch('/pgn_txt/' + filename);
   const fileContent = await response.text();
   return fileContent.split('\n\n').filter(pgn => pgn.trim() !== '');
 }
